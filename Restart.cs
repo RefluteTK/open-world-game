9cf0cfb854d95ee4a3291ac3f731717c350787d0
Tinoisthe <76804686+Tinoisthe@users.noreply.github.com>
2021-04-05 13:45:09 +1000

    Update Restart.cs

--- Restart.cs
+++ Restart.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     public void RestartGame()
-{
-     SceneManager.LoadScene("newGameScene");
+{                             
+     SceneManager.LoadScene("Add scene Name here");
 }
 }
