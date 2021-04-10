c2932f3700e679bcf2c3c77ad8962469d119e1ac
Tinoisthe <76804686+Tinoisthe@users.noreply.github.com>
2021-04-10 18:29:54 +1000

    Add a shield to cover the HP
    Updated

--- Target.cs
+++ Target.cs
@@ -1,11 +1,18 @@
-﻿
-using UnityEngine;
 
+using UnityEngine;
+// The name of the Target can be change
 public class Target : MonoBehaviour
 {
-   public float health = 50f;
+// can be change
+   public float health = 100f;
+   public float shield = 100f;
    public void  TakeDamage (float amount)
+   
    {
+
+        shield -= amount;
+       if (shield <= 0f)
+       
        health -= amount;
        if (health <= 0f)
        {
